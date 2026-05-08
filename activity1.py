6e8f1b912725bc3a4ff392b56aad991e0b5d896f
Shreyas-2112 <sompurshreyas@gmail.com>
2026-05-08 15:40:23 +0530

    Add files via upload

---
+++ activity1.py
@@ -0,0 +1,12 @@
+flower1 = 88
+flower2 = 67
+flower3 = 95
+flower4 = 96
+flower5 = 97
+flower6 = 99
+
+sum = flower1+flower2+flower3+flower4+flower5+flower6
+print("the sum of all the 6 flowers is: " ,sum)
+Average = sum/6
+
+print("the Average of all the 6 flower is: ", Average)
